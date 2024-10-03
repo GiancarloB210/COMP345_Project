487c22db4b6fd01918db47a1dde2635bd3b1f8b8
Giancarlo <g_biasiucci@outlook.com>
2024-10-02 23:19:52 -0400

    Added stream insertion/output, copy constructor and assignment operators to CardsDriver, added comments to CardsDriver

--- CardsDriver.h
+++ CardsDriver.h
@@ -5,6 +5,10 @@
 class CardsDriver {
 public:
     CardsDriver();
+    CardsDriver(CardsDriver& driver);
+    friend ostream& operator <<(ostream& out_stream, CardsDriver& driver);
+    friend istream& operator >>(istream& in_stream, CardsDriver& driver);
+    CardsDriver& operator=(CardsDriver driver);
 
     int countDrawableCards(Hand *hand);
 
