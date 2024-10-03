928ec1184b7faa4914ba02d97dd4f0004aaaee66
Giancarlo <g_biasiucci@outlook.com>
2024-10-03 12:05:58 -0400

    re-adding cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/COMP345_Project.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sabinacolacci/Desktop/COMP345_Project/Cards.cpp" "CMakeFiles/COMP345_Project.dir/Cards.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/Cards.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/CardsDriver.cpp" "CMakeFiles/COMP345_Project.dir/CardsDriver.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/CardsDriver.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/GameEngine.cpp" "CMakeFiles/COMP345_Project.dir/GameEngine.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/GameEngine.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/GameEngineDriver.cpp" "CMakeFiles/COMP345_Project.dir/GameEngineDriver.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/GameEngineDriver.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/MainDriver.cpp" "CMakeFiles/COMP345_Project.dir/MainDriver.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/MainDriver.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/Map.cpp" "CMakeFiles/COMP345_Project.dir/Map.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/Map.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/MapDriver.cpp" "CMakeFiles/COMP345_Project.dir/MapDriver.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/MapDriver.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/Orders.cpp" "CMakeFiles/COMP345_Project.dir/Orders.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/Orders.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/OrdersDriver.cpp" "CMakeFiles/COMP345_Project.dir/OrdersDriver.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/OrdersDriver.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/Player.cpp" "CMakeFiles/COMP345_Project.dir/Player.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/Player.cpp.o.d"
+  "/Users/sabinacolacci/Desktop/COMP345_Project/PlayerDriver.cpp" "CMakeFiles/COMP345_Project.dir/PlayerDriver.cpp.o" "gcc" "CMakeFiles/COMP345_Project.dir/PlayerDriver.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/COMP345_Project.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COMP345_Project.dir/Cards.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/Cards.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/CardsDriver.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/CardsDriver.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/GameEngine.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/GameEngine.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/GameEngineDriver.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/GameEngineDriver.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/MainDriver.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/MainDriver.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/Map.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/Map.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/MapDriver.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/MapDriver.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/Orders.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/Orders.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/OrdersDriver.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/OrdersDriver.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/Player.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/Player.cpp.o.d"
+  "CMakeFiles/COMP345_Project.dir/PlayerDriver.cpp.o"
+  "CMakeFiles/COMP345_Project.dir/PlayerDriver.cpp.o.d"
+  "COMP345_Project"
+  "COMP345_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COMP345_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
