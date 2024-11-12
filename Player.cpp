1635eb292f0ec1df9b12e7cd62c62fc8d45e1297
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-11-11 20:13:37 -0500

    Initialize reinforcement pool in Player class and add accessor methods

--- Player.cpp
+++ Player.cpp
@@ -11,6 +11,7 @@ Player::Player(std::list<Territory*>* territories, Hand* hand) {
     this->hand = new Hand(*hand);
     this->orders = new OrderList();
     this->armyUnits = std::vector<ArmyUnit*>();
+    this->reinforcementPool = 0; // Initialize reinforcement pool
 }
 
 // Parameterized constructor (with name input)
@@ -22,6 +23,7 @@ Player::Player(std::string newName, std::list<Territory*>* territories, Hand* ha
     this->hand = new Hand(*hand);
     this->orders = new OrderList();
     this->armyUnits = std::vector<ArmyUnit*>();
+    this->reinforcementPool = 0; // Initialize reinforcement pool
 }
 
 // Copy constructor (deep copy)
@@ -33,6 +35,7 @@ Player::Player(const Player& player) {
     this->hand = new Hand(*player.hand);
     this->orders = new OrderList(*player.orders);
     this->armyUnits = std::vector<ArmyUnit*>(player.armyUnits);
+    this->reinforcementPool = player.reinforcementPool;
 }
 
 // Destructor to clean up dynamic memory
@@ -43,7 +46,7 @@ Player::~Player() {
     this->hand = NULL;
     delete this->orders;
     this->orders = NULL;
-    for (int i = 0;i < this->armyUnits.size();i++) {
+    for (int i = 0; i < this->armyUnits.size(); i++) {
         delete this->armyUnits[i];
         this->armyUnits[i] = NULL;
     }
@@ -66,14 +69,15 @@ Player& Player::operator=(const Player& player) {
         this->hand = new Hand(*player.hand);
         this->orders = new OrderList(*player.orders);
         this->armyUnits = std::vector<ArmyUnit*>(player.armyUnits);
+        this->reinforcementPool = player.reinforcementPool;
     }
     return *this;
 }
 
 // Stream insertion operator for printing player information
 std::ostream& operator<<(std::ostream& out, const Player& player) {
-    out << "Player's ID: " << player.playerID << endl;
-    out << "Player's name: " << player.name << endl;
+    out << "Player's ID: " << player.playerID << std::endl;
+    out << "Player's name: " << player.name << std::endl;
     out << "Player's territories to defend: ";
     for (Territory* t : *(player.territories)) {
         out << t->getName() << " ";
@@ -82,6 +86,15 @@ std::ostream& operator<<(std::ostream& out, const Player& player) {
     out << "\nPlayer's list of orders: " << *(player.orders);
     out << "\nPlayer's number of army units: " << player.armyUnits.size();
     return out;
+}
+
+// Accessor methods
+std::string Player::getName() const {
+    return name;
+}
+
+const std::list<Territory*>& Player::getTerritories() const {
+    return *territories;
 }
 
 // Method to return a list of all territories to defend (returns the pointer to the list)
@@ -113,4 +126,29 @@ void Player::issueOrder() {
     Order* newOrder = new DeployOrder();
     orders->add(newOrder);
     std::cout << "Issued a DeployOrder.\n";
+}
+
+// Adds units to the reinforcement pool
+void Player::addToReinforcementPool(int units) {
+    reinforcementPool += units;
+}
+
+// Checks if the player has more orders to issue or execute
+bool Player::hasMoreOrders() const {
+    return !orders->getList().empty();
+}
+
+// Checks if the player owns any territory
+bool Player::ownsTerritory() const {
+    return !territories->empty();
+}
+
+// Retrieves and removes the next order from the player's orders list
+Order* Player::getNextOrder() {
+    if (!orders->getList().empty()) {
+        Order* nextOrder = orders->getList().front();
+        orders->getList().erase(orders->getList().begin());
+        return nextOrder;
+    }
+    return nullptr; // Return nullptr if there are no orders
 }
